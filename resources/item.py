17354cc218f0e2395a87719f647129a94e49a3c8
Emiliano Famá <wolksvidia@gmail.com>
2019-01-21 14:09:40 -0300

    section6

---
+++ resources/item.py
@@ -0,0 +1,73 @@
+from flask import request
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+from models.item import ItemModel
+
+class Item(Resource):
+    
+    @staticmethod
+    def _parser(message):
+        parser = reqparse.RequestParser() #parsear los datos que vienen en el JSON, solo se admiten los declarados, el resto se borra
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help=message
+        )
+        parser.add_argument('store_id',
+            type=int,
+            required=True,
+            help=message
+        )
+        return parser.parse_args()
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+
+        if item:
+            return item.json()
+        return {'message': 'item not found'}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': 'El item de nombre {} ya existe'.format(name)}, 400
+        
+        data = self._parser('Campo requerido!')
+
+        item = ItemModel(name, data['price'], data['store_id'])
+
+        try:
+            item.save()
+        except:
+            return {'message': 'Un ERROR ocurrio'}, 500
+        return item.json(), 201
+
+    def put(self, name):
+        data = self._parser('Campo requerido!')
+        item = ItemModel.find_by_name(name)
+        
+        if item:
+            item.price = data['price']
+        else:
+            item = ItemModel(name, **data)
+        
+        try:
+            item.save()
+        except:
+            return {'message': 'Un ERROR ocurrio'}, 500
+        
+        return item.json(), 201
+
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete()
+        return {'message': 'Item delete'}
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': [i.json() for i in ItemModel.query.all()]}
+        #whith lambda
+        #return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
---
+++ resources/store.py
@@ -0,0 +1,37 @@
+
+from flask_restful import Resource
+
+from models.store import StoreModel
+
+
+class Store(Resource):
+    
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+
+        if store:
+            return store.json()
+        return {'message': 'store not found'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': 'El store de nombre {} ya existe'.format(name)}, 400
+
+        store = StoreModel(name)
+
+        try:
+            store.save()
+        except:
+            return {'message': 'Un ERROR ocurrio'}, 500
+        return store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete()
+        return {'message': 'store delete'}
+
+
+class StoreList(Resource):
+    def get(self):
+        return {'Stores': [store.json() for store in StoreModel.query.all()]}
